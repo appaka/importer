02a7419e144381d86425697fe6a89e97ece19e9a
Javier Pérez <javier.perez@ufirstgroup.ch>
2020-02-05 13:24:35 +0100

    CSV reader

--- src/reader.csv.go
+++ src/reader.csv.go
@@ -8,84 +8,118 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 	"time"
 )
 
-func getRow(reader *csv.Reader, headers []string) (*string, error) {
-	record, err := reader.Read()
+var config struct {
+	CsvFilePath     string
+	ProcessFilePath string
+	DeleteOthers    bool
+}
+
+var csvFile struct {
+	Reader  *csv.Reader
+	Headers []string
+	EOF     bool
+	Items   int64
+	Skipped int64
+}
+
+func getDocument() *string {
+	record, err := csvFile.Reader.Read()
+	if err == io.EOF {
+		csvFile.EOF = true
+		return nil
+	}
 	if err != nil {
-		return nil, err
+		csvFile.Skipped++
+		return nil
 	}
 
+	// transform the array into a map
 	row := map[string]string{}
-	if headers != nil {
-		// transform the array into a map
-		for i := 0; i < len(headers); i++ {
-			var field string = headers[i]
-			var value = record[i]
-			row[field] = value
-		}
+	for i := 0; i < len(csvFile.Headers); i++ {
+		var field string = csvFile.Headers[i]
+		var value string = record[i]
+		row[field] = value
 	}
 
 	// transform the map into a JSON object
 	document, err := json.Marshal(row)
 	if err != nil {
-		return nil, err
+		csvFile.Skipped++
+		return nil
 	}
-	documentString := string(document)
 
-	return &documentString, nil
+	csvFile.Items++
+
+	documentString := string(document)
+	return &documentString
 }
 
-func openFile(filename string) (reader *csv.Reader, headers []string) {
-	stream, error := os.Open(filename)
+func openCSVFile() {
+	stream, error := os.Open(config.CsvFilePath)
 	if error != nil {
 		fmt.Printf("ERROR!")
 		os.Exit(1)
 	}
 
-	reader = csv.NewReader(bufio.NewReader(stream))
+	csvFile.Reader = csv.NewReader(bufio.NewReader(stream))
 
-	headers, err := reader.Read()
+	headers, err := csvFile.Reader.Read()
 	if err == io.EOF {
 		os.Exit(1)
 	}
 
-	return reader, headers
+	csvFile.Headers = headers
+	csvFile.EOF = false
+	csvFile.Items = 0
+	csvFile.Skipped = 0
+}
+
+func h1(title string) {
+	fmt.Printf("%s %s\n", title, strings.Repeat("=", 60-len(title)-1))
+}
+
+func loadConfig() {
+	// PARAMS
+	csvFilePath := flag.String("file", "", "file path to be imported")
+	processFilePath := flag.String("process", "", "process script file path")
+	deleteOthers := flag.Bool("delete-others", false, "delete items not presents in this file")
+	flag.Parse()
+
+	config.CsvFilePath = *csvFilePath
+	config.ProcessFilePath = *processFilePath
+	config.DeleteOthers = *deleteOthers
+
+	mapBooleanYesNo := map[bool]string{
+		false: "No",
+		true:  "Yes",
+	}
+
+	h1("CONFIGURATION")
+	fmt.Printf(" --file (CSV file path)                 = %s\n", config.CsvFilePath)
+	fmt.Printf(" --process (Processor script file path) = %s\n", config.ProcessFilePath)
+	fmt.Printf(" --delete-others (Delete others items)  = %s\n", mapBooleanYesNo[config.DeleteOthers])
+	fmt.Println()
 }
 
 func main() {
-	// PARAMS
-	filename := flag.String("file", "", "file path to be imported")
-	preprocessorScript := flag.String("process", "", "process script file path")
-	flag.Parse()
-
-	fmt.Printf("File: %s\n", *filename)
-	fmt.Printf("Script: %s\n", *preprocessorScript)
-
-	// LOAD FILE
-	reader, headers := openFile(*filename)
+	loadConfig()
+	openCSVFile()
 
 	start := time.Now()
-	counter := float64(0)
-	skipped := int16(0)
 
 	for {
-		counter++
-
 		// transform the array into a map
-		document, err := getRow(reader, headers)
-		if err == io.EOF {
+		document := getDocument()
+		if csvFile.EOF {
 			break
 		}
 		if document == nil {
-			skipped++
 			continue
 		}
-
-		// print it
-		//fmt.Printf("%.0f ", counter)
-		//fmt.Println(*document)
 
 		/*
 			TODO: execute reader-processor, which will return the final document
@@ -118,8 +152,9 @@ func main() {
 
 	// final stats
 	elapsedSeconds := time.Now().Sub(start).Seconds()
-	fmt.Printf("   processed: %14.0f (%d skipped / %.2f%%)\n", counter, skipped, (float64(skipped)/counter)*100)
-	fmt.Printf("time elapsed: %14.4f\n", elapsedSeconds)
-	fmt.Printf(" rows/second: %14.4f\n", counter/elapsedSeconds)
-	fmt.Printf("files/second: %14.4f\n", (counter/elapsedSeconds)/counter)
+	h1("STATS")
+	fmt.Printf("    processed: %14d (%d skipped / %.2f%%)\n", csvFile.Items, csvFile.Skipped, float64(csvFile.Skipped/csvFile.Items)*100)
+	fmt.Printf(" time elapsed: %14.4f\n", elapsedSeconds)
+	fmt.Printf("  rows/second: %14.4f\n", float64(csvFile.Items)/elapsedSeconds)
+	fmt.Printf(" files/second: %14.4f\n", (float64(csvFile.Items)/elapsedSeconds)/float64(csvFile.Items))
 }
