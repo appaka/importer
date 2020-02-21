5c8ef2c1a29b2904874b8ee828f3573dd0f7e2dc
Javier Pérez <javier.perez@ufirstgroup.ch>
2020-02-21 16:47:33 +0100

    new config loader

--- src/config/config.go
+++ src/config/config.go
@@ -1,11 +1,17 @@
 package config
 
 import (
+	"flag"
 	"os"
 )
 
 type Config struct {
 	DBConfig *DBConfig
+
+	CsvFilePath     string
+	ProcessFilePath string
+	FullImport      bool
+	Debug           bool
 }
 
 type DBConfig struct {
@@ -25,14 +31,33 @@ func getenv(key, fallback string) string {
 }
 
 func GetConf() *Config {
-	return &Config{
-		DBConfig: &DBConfig{
-			Dialect:  getenv("TODO_DB_DIALECT", "postgres"),
-			Name:     getenv("TODO_DB_NAME", "tododb"),
-			Username: getenv("TODO_DB_USERNAME", "tododb"),
-			Password: getenv("TODO_DB_PASSWORD", "tododb"),
-			Host:     getenv("TODO_DB_HOST", "localhost"),
-			Port:     getenv("TODO_DB_PORT", "5432"),
-		},
+	config := Config{
+		CsvFilePath:     getenv("APPAKA_CSV_FILE_PATH", ""),
+		ProcessFilePath: getenv("APPAKA_PROCESS_FILE_PATH", ""),
+		FullImport:      getenv("APPAKA_FULL_IMPOTY", "0"),
+		Debug:           getenv("APPAKA_DEBUG", "1"),
 	}
+
+	// PARAMS
+	csvFilePath := flag.String("file", "", "file path to be imported")
+	processFilePath := flag.String("process", "", "process script file path")
+	fullImport := flag.Bool("full-import", false, "delete items not presents in this file")
+	debug := flag.Bool("debug", true, "debug process")
+	flag.Parse()
+
+	config.CsvFilePath = *csvFilePath
+	config.ProcessFilePath = *processFilePath
+	config.FullImport = *fullImport
+	config.Debug = *debug
+
+	config.DBConfig = &DBConfig{
+		Dialect:  getenv("TODO_DB_DIALECT", "postgres"),
+		Name:     getenv("TODO_DB_NAME", "tododb"),
+		Username: getenv("TODO_DB_USERNAME", "tododb"),
+		Password: getenv("TODO_DB_PASSWORD", "tododb"),
+		Host:     getenv("TODO_DB_HOST", "localhost"),
+		Port:     getenv("TODO_DB_PORT", "5432"),
+	}
+
+	return &config
 }
